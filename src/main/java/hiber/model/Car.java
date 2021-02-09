f8fb922fe3e95373d043d13fef47d152bdba07bb
Niloniol <hellorock@mail.ru>
2021-02-10 02:31:14 +0700

    It just works!

---
+++ src/main/java/hiber/model/Car.java
@@ -0,0 +1,54 @@
+package hiber.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "model")
+    private String model;
+
+    @Column(name = "series")
+    private String series;
+
+    @OneToOne(mappedBy = "car")
+    private User user;
+
+    public Car(String model, String series) {
+        this.model = model;
+        this.series = series;
+    }
+
+    public Car() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getSeries() {
+        return series;
+    }
+
+    public void setSeries(String series) {
+        this.series = series;
+    }
+}
